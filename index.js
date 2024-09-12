79e82ccdb6a5e6f8e3fcf4cb066fc2a1509da8d1
Weskley Bezerra <weskleymb@users.noreply.github.com>
2024-09-12 00:44:45 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// Crie um programa que receba um nome e imprima a mensagem “Bem vindo [nome]”
 const prompt = require('prompt-sync')();
 
+// Entrada
 const valor = prompt("Digite: ");
 
+// Saída
 console.log(`Bem vindo ${valor}`);
